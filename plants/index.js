239f4c7d4a4d8e0b1f512625dc52211554a58997
Nara_Safarian <nara.safaryan@gmail.com>
2023-02-06 00:46:53 +0700

    add: js logics and css

--- plants/index.js
+++ plants/index.js
@@ -1,31 +1,81 @@
-console.log(
-    'Вёрстка соответствует макету. Ширина экрана 768px +24\n'
-    + 'блок <header> +2\n'
-    + 'секция welcome +3\n'
-    + 'секция about +4\n'
-    + 'секция service +4\n'
-    + 'секция prices +4\n'
-    + 'секция contacts +4\n'
-    + 'блок <footer> + 3\n'
-    
-    + 'Вёрстка соответствует макету. Ширина экрана 380px +24\n'
-    + 'блок <header> +2\n'
-    + 'секция welcome +3\n'
-    + 'секция about +4\n'
-    + 'секция service +4\n'
-    + 'секция prices +4\n'
-    + 'секция contacts +4\n'
-    + 'блок <footer> + 3\n'
-    + 'Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +15\n'
-    + 'нет полосы прокрутки при ширине страницы от 1440рх до 380px +7\n'
-    + 'нет полосы прокрутки при ширине страницы от 380px до 320рх +8\n'
-    + 'На ширине экрана 380рх и меньше реализовано адаптивное меню +22 (Допускается появление адаптивного меня на ширине более 380, но не допускается на ширине более 770px)\n'
-    + 'при ширине страницы 380рх панель навигации скрывается, появляется бургер-иконка +2\n'
-    + 'при нажатии на бургер-иконку плавно появляется адаптивное меню +4\n'
-    + 'адаптивное меню соответствует цветовой схеме макета +4\n'
-    + 'при нажатии на крестик адаптивное меню плавно скрывается уезжая за экран +4\n'
-    + 'ссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям +4\n'
-    + 'при клике по ссылке в адаптивном меню адаптивное меню плавно скрывается, также скрытие меню происходит если сделать клик вне данного окна +4\n'
+const serviceButtons = document.querySelector('.service__buttons');
+const serviceCells = document.querySelector('.service__list');
 
-    + 'Итого 75 баллов'
-)
+
+const MAX_NUMBER_OF_SELECTED_BUTTONS = 2;
+let numberOfSelectedButtons = 0;
+const selectedCatergories = new Set();
+
+
+window.onload = function() {
+    addBtnClickHandler();
+    blurring();
+}
+
+const clickHandler = (e) => {
+    const clickedButton = e.target;
+
+    if (clickedButton.classList.contains('service__btn')) {
+        if (clickedButton.classList.contains('active')) {
+            removeSelectedButtons(clickedButton);
+        } else {
+            if (numberOfSelectedButtons < MAX_NUMBER_OF_SELECTED_BUTTONS) {
+                selectClickedButton(clickedButton);
+            }
+        }
+    }
+
+    blurring();
+};
+
+const addBtnClickHandler = () => {
+    serviceButtons.addEventListener('click', clickHandler);
+}
+
+const selectClickedButton = (clickedButton) => {
+    clickedButton.classList.add('active');
+    numberOfSelectedButtons++;
+    selectedCatergories.add(clickedButton.value);
+} 
+
+const removeSelectedButtons = (removedButton) => {
+    removedButton.classList.remove('active');
+    numberOfSelectedButtons--;
+    selectedCatergories.delete(removedButton.value);
+}
+
+const blurring = () => {
+    if (numberOfSelectedButtons === 0) {
+        for (let cell of serviceCells.children) {
+            cell.classList.remove('blur');
+        }
+        return;
+    }
+
+    for (let cell of serviceCells.children) {
+        const category = cell.dataset.category;
+        if (selectedCatergories.has(category)) {
+            cell.classList.remove('blur');
+        } else {
+            cell.classList.add('blur');
+        }
+    }
+};
+
+
+// -------- accordion
+
+const acc = document.getElementsByClassName("packages__accordion");
+
+for (let i = 0; i < acc.length; i++) {
+    const clickHandler = function() {
+        this.classList.toggle("active");
+        const panel = this.children[1];
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+    acc[i].addEventListener("click", clickHandler);
+}
